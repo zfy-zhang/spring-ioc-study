741b99763ed6dbb820a2a93898dc014dddba513c
zfy-zhang <zfy_zang@163.com>
2020-09-06 20:52:49 +0800

    commit

--- thinking-in-spring/bean-lifecycle/src/main/java/com/pat/thinking/in/spring/bean/lifecycle/MergedBeanDefinitionDemo.java
+++ thinking-in-spring/bean-lifecycle/src/main/java/com/pat/thinking/in/spring/bean/lifecycle/MergedBeanDefinitionDemo.java
@@ -1,11 +1,35 @@
 package com.pat.thinking.in.spring.bean.lifecycle;
 
+import com.pat.thinking.in.spring.ioc.overview.domain.User;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.EncodedResource;
+
 /**
- * @Description: BeanDefinition 合并示例 
+ * @Description: BeanDefinition 合并示例
  * @Author <a href="mailto:zfy_zang@163.com">Vincent</a>
  * @Create 2020/9/5
  * @Modify
  * @since
  */
 public class MergedBeanDefinitionDemo {
+    public static void main(String[] args) {
+        DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+        XmlBeanDefinitionReader beanDefinitionReader = new XmlBeanDefinitionReader(beanFactory);
+        String location = "META-INF/dependency-lookup-context.xml";
+        // 基于 classpath 加载 properties 资源
+        Resource resource = new ClassPathResource(location);
+        // 指定字符编码格式
+        EncodedResource encodedResource = new EncodedResource(resource, "UTF-8");
+        int beanNumbers = beanDefinitionReader.loadBeanDefinitions(encodedResource);
+        System.out.println("已加载 BeanDefinition 数量 ： " + beanNumbers);
+        // 通过 Bean Id 和类型进行依赖查找
+        User user = beanFactory.getBean("user", User.class);
+        System.out.println(user);
+
+        User superUser = beanFactory.getBean("superUser", User.class);
+        System.out.println(superUser);
+    }
 }
