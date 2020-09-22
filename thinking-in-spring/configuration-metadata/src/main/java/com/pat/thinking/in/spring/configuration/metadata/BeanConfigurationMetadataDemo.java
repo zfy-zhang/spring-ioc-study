4c41a3c50e322dcb8350f98b11617e24d73aa161
zfy-zhang <zfy_zang@163.com>
2020-09-22 23:59:42 +0800

    commit

--- thinking-in-spring/configuration-metadata/src/main/java/com/pat/thinking/in/spring/configuration/metadata/BeanConfigurationMetadataDemo.java
+++ thinking-in-spring/configuration-metadata/src/main/java/com/pat/thinking/in/spring/configuration/metadata/BeanConfigurationMetadataDemo.java
@@ -30,6 +30,9 @@ public class BeanConfigurationMetadataDemo {
         // 附加属性 （不影响 Bean 实例化、属性赋值、初始化）
         beanDefinition.setAttribute("name", "pat");
 
+        // 当前 BeanDefinition 来自何方（辅助作用）
+        beanDefinition.setSource(BeanConfigurationMetadataDemo.class);
+
         DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
         
         beanFactory.addBeanPostProcessor(new BeanPostProcessor() {
@@ -37,10 +40,12 @@ public class BeanConfigurationMetadataDemo {
             public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
                 if (ObjectUtils.nullSafeEquals("user", beanName) && User.class.equals(bean.getClass())) {
                     BeanDefinition bd = beanFactory.getBeanDefinition(beanName);
-                    // 属性（存储）上下文
-                    String name = (String) bd.getAttribute("name");
-                    User user = (User) bean;
-                    user.setName(name);
+                    if (BeanConfigurationMetadataDemo.class.equals(bd.getSource())) { // 通过 source 来判断
+                        // 属性（存储）上下文
+                        String name = (String) bd.getAttribute("name");
+                        User user = (User) bean;
+                        user.setName(name);
+                    }
                 }
                 return bean;
             }
