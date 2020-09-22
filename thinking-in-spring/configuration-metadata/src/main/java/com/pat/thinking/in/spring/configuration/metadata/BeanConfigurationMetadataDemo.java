6e1439fa5c57e21f8a81e948686acabf3977a31d
zfy-zhang <zfy_zang@163.com>
2020-09-22 23:53:39 +0800

    commit

---
+++ thinking-in-spring/configuration-metadata/src/main/java/com/pat/thinking/in/spring/configuration/metadata/BeanConfigurationMetadataDemo.java
@@ -0,0 +1,54 @@
+package com.pat.thinking.in.spring.configuration.metadata;
+
+import com.pat.thinking.in.spring.ioc.overview.domain.User;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.beans.factory.support.AbstractBeanDefinition;
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.util.ObjectUtils;
+
+/**
+ * @Description: Bean 配置元信息示例
+ * @Author <a href="mailto:zfy_zang@163.com">Vincent</a>
+ * @Create 2020/9/22
+ * @Modify
+ * @since
+ */
+public class BeanConfigurationMetadataDemo {
+
+    public static void main(String[] args) {
+
+
+        // BeanDefinition 的定义（声明）
+        BeanDefinitionBuilder beanDefinitionBuilder = BeanDefinitionBuilder.genericBeanDefinition(User.class);
+        beanDefinitionBuilder.addPropertyValue("name", "不才人");
+
+        // 获取 AbstractBeanDefinition
+        AbstractBeanDefinition beanDefinition = beanDefinitionBuilder.getBeanDefinition();
+        // 附加属性 （不影响 Bean 实例化、属性赋值、初始化）
+        beanDefinition.setAttribute("name", "pat");
+
+        DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+        
+        beanFactory.addBeanPostProcessor(new BeanPostProcessor() {
+            @Override
+            public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+                if (ObjectUtils.nullSafeEquals("user", beanName) && User.class.equals(bean.getClass())) {
+                    BeanDefinition bd = beanFactory.getBeanDefinition(beanName);
+                    // 属性（存储）上下文
+                    String name = (String) bd.getAttribute("name");
+                    User user = (User) bean;
+                    user.setName(name);
+                }
+                return bean;
+            }
+        });
+        // 注册 User 的 BeanDefinition
+        beanFactory.registerBeanDefinition("user", beanDefinition);
+
+        User user = beanFactory.getBean("user", User.class);
+        System.out.println(user);
+    }
+}
